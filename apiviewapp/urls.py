1941301e5f917e844b7078342eb5b3f7203e214d
Aljoboyer <tanveeazmain@gmail.com>
2024-09-05 11:12:23 +0600

    Patch And Put Done

--- apiviewapp/urls.py
+++ apiviewapp/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('movies/', views.Allmovies),
     path('movie-create/', views.Create_Movie),
+    path('movie-update/<int:pk>', views.Update_Movie),
+    path('movie-patch/<int:pk>', views.Patch_Movie),
+
 ]
--- apiviewapp/views.py
+++ apiviewapp/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from . serializers import MovieSerializer
-from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
 from . models import Movie
-from django.views.decorators.csrf import csrf_exempt
-import io
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -27,5 +23,32 @@ def Create_Movie(request):
         res = {'msg': 'Successfully Insert Done'}
 
         return Response(res)
+    
+    return Response(serializer.errors)
+
+@api_view(['PUT'])
+def Update_Movie(request, pk=None):
+    print('Just Checking', request.data, pk)
+    id = pk
+    movie = Movie.objects.get(pk=id)
+
+    serializer = MovieSerializer(movie, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'msg': 'Full Data Updated'})
+    
+    return Response(serializer.errors)
+
+@api_view(['PATCH'])
+def Patch_Movie(request, pk=None):
+    id = pk
+    movie = Movie.objects.get(pk=id)
+
+    serializer = MovieSerializer(movie, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'msg': 'Partial Data Updated'})
     
     return Response(serializer.errors)
