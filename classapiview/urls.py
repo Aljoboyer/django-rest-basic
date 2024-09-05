451f99667ddf10231c53f3aa4421cf3ce7de7b21
Aljoboyer <tanveeazmain@gmail.com>
2024-09-05 21:19:01 +0600

    class api view started

--- classapiview/urls.py
+++ classapiview/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/', views.Allbook),
-    path('book-create/', views.Create_Book),
-    path('book-update/<int:pk>', views.Update_Book),
-    path('book-patch/<int:pk>', views.Patch_Book),
+    path('book/', views.Allbook.as_view()),
+    path('book-create/', views.Create_Book.as_view()),
+    path('book-update/<int:pk>', views.Update_Book.as_view()),
+    path('book-patch/<int:pk>', views.Patch_Book.as_view()),
 
 ]
--- classapiview/views.py
+++ classapiview/views.py
@@ -2,53 +2,51 @@ from django.shortcuts import render
 from . serializers import BookSerializer
 from . models import Book
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
+from rest_framework.decorators import APIView
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def Allbook(request):
-    ai = Book.objects.all()
+class Allbook(APIView):
+    def get(self, request):
+        ai = Book.objects.all()
 
-    serializer = BookSerializer(ai, many = True)
+        serializer = BookSerializer(ai, many = True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def Create_Book(request):
+class Create_Book(APIView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Successfully Insert Done'}
+
+            return Response(res)
+        
+        return Response(serializer.errors)
+
+class Update_Book(APIView):
+    def put(self, request, pk= None):
+        id = pk
+        books = Book.objects.get(pk=id)
+
+        serializer = BookSerializer(books, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Full Data Updated'})
+        
+        return Response(serializer.errors)
     
-    serializer = BookSerializer(data=request.data)
+class Patch_Book(APIView):
+    def patch(self, request, pk= None):
+        id = pk
+        Books = Book.objects.get(pk=id)
 
-    if serializer.is_valid():
-        serializer.save()
-        res = {'msg': 'Successfully Insert Done'}
+        serializer = BookSerializer(Books, data=request.data, partial=True)
 
-        return Response(res)
-    
-    return Response(serializer.errors)
-
-@api_view(['PUT'])
-def Update_Book(request, pk=None):
-    print('Just Checking', request.data, pk)
-    id = pk
-    Book = Book.objects.get(pk=id)
-
-    serializer = BookSerializer(Book, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'msg': 'Full Data Updated'})
-    
-    return Response(serializer.errors)
-
-@api_view(['PATCH'])
-def Patch_Book(request, pk=None):
-    id = pk
-    Book = Book.objects.get(pk=id)
-
-    serializer = BookSerializer(Book, data=request.data, partial=True)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'msg': 'Partial Data Updated'})
-    
-    return Response(serializer.errors)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Partial Data Updated'})
+        
+        return Response(serializer.errors)
