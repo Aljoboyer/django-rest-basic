10dee1cc984cdda84e55601d551905d74d06262e
Aljoboyer <tanveeazmain@gmail.com>
2024-09-12 11:39:39 +0600

    partial update mixing done

--- mixinapp/urls.py
+++ mixinapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('userList/', views.UserList.as_view()),
     path('createUser/', views.CreateUser.as_view()),
+    path('userretrive/<int:pk>', views.UserRetrive.as_view()),
+    path('userUpdate/<int:pk>', views.UserUpdate.as_view()),
+    path('userpatch/<int:pk>', views.UserPartialUpdate.as_view()),
 
 ]
--- mixinapp/views.py
+++ mixinapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from . serializers import UserSerializer
 from . models import User
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 
 # Create your views here.
 class UserList(GenericAPIView, ListModelMixin):
@@ -18,3 +18,25 @@ class CreateUser(GenericAPIView, CreateModelMixin):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+class UserRetrive(GenericAPIView, RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+class UserUpdate(GenericAPIView, UpdateModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+class UserPartialUpdate(GenericAPIView, UpdateModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def put(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
